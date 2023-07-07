3c3d933b71ac52f70af183fbbd567a378ce68d4e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-07-07 17:32:14 +0100

    little by little

--- FirstJava/main.java
+++ FirstJava/main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+ String fname = "John";
+ String lname = "Doe";
+ int age = 24;
+
+ static void myMethod(String[] args) {
+    System.out.println("Hello World!");
+ }
+ public static void main(String[] args) {
+    System.out.println("Hello World!");
+ Main myObj = new Main();
+ System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+ System.out.println("Age: " + myObj.age);
+ }
 }
